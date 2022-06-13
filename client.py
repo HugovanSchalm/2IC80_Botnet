d774b0b1e4e763aec44c7b6711e485e1f6ad0295
Hugo van Schalm <hugovanschalm@hotmail.com>
2022-06-13 19:28:31 +0200

    Client registering

--- client.py
+++ client.py
@@ -1,1 +1,16 @@
+import socket
+import sys
+import selectors
+import types
+
 # CODE THAT RUNS ON INFECTED MACHINE WILL GO HERE
+
+HOST = "127.0.0.1"
+PORT = 1234
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
--- server.py
+++ server.py
@@ -1,1 +1,32 @@
+import socket
+import sys
+
 # COMMAND AND CONTROL CODE WILL GO HERE
+
+slaves = list()
+
+port = 1234
+ip = '127.0.0.1'
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('127.0.0.1', port))
+    print(f"Bound to {ip}:{str(port)}")
+    
+    s.listen()
+    s.settimeout(5)
+    print("Listening")
+    try:
+        while True:
+            try:
+                sres, addr = s.accept()
+                print(f'Got connection from {addr}')
+                sres.sendall(b"Jo dankjewel voor je berichtje")
+                sres.close()
+                if slaves.count(addr) == 0:
+                    slaves.append(addr)
+                    with open('slaves.txt', 'w') as file:
+                        file.write(f"{addr}\n")
+            except KeyboardInterrupt:
+                pass
+    except KeyboardInterrupt:
+        pass
